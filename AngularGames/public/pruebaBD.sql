80c66b500501ed871214ebc416e3abc1d35eb922
Pendergastt <penddergast@gmail.com>
2025-05-21 19:20:18 +0200

    añadidos modelos y prueba base datos

---
+++ AngularGames/public/pruebaBD.sql
@@ -0,0 +1,237 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: juegosdemesa
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administradores`
+--
+
+DROP TABLE IF EXISTS `administradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administradores` (
+  `idAdministrador` varchar(20) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`idAdministrador`),
+  UNIQUE KEY `idAdministrador_UNIQUE` (`idAdministrador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administradores`
+--
+
+LOCK TABLES `administradores` WRITE;
+/*!40000 ALTER TABLE `administradores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `administradores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `almacen`
+--
+
+DROP TABLE IF EXISTS `almacen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `almacen` (
+  `idArticulo` varchar(20) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `cantidad` int NOT NULL,
+  `almacencol` varchar(45) NOT NULL,
+  PRIMARY KEY (`idArticulo`),
+  UNIQUE KEY `idArticulo_UNIQUE` (`idArticulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `almacen`
+--
+
+LOCK TABLES `almacen` WRITE;
+/*!40000 ALTER TABLE `almacen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `almacen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `articulos`
+--
+
+DROP TABLE IF EXISTS `articulos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `articulos` (
+  `idarticulo` varchar(20) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idarticulo`),
+  UNIQUE KEY `idarticulo_UNIQUE` (`idarticulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articulos`
+--
+
+LOCK TABLES `articulos` WRITE;
+/*!40000 ALTER TABLE `articulos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articulos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historial pedidos`
+--
+
+DROP TABLE IF EXISTS `historial pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial pedidos` (
+  `idPedido` int NOT NULL AUTO_INCREMENT,
+  `numPedido` int NOT NULL,
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`idPedido`),
+  UNIQUE KEY `idPedido_UNIQUE` (`idPedido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial pedidos`
+--
+
+LOCK TABLES `historial pedidos` WRITE;
+/*!40000 ALTER TABLE `historial pedidos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historial pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `juegos`
+--
+
+DROP TABLE IF EXISTS `juegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juegos` (
+  `idArticulo` varchar(20) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Tipo` varchar(45) NOT NULL,
+  `jugadoresMin` int NOT NULL,
+  `jugadoresMax` int NOT NULL,
+  `dificultad` int NOT NULL,
+  PRIMARY KEY (`idArticulo`),
+  UNIQUE KEY `idJuegos_UNIQUE` (`idArticulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juegos`
+--
+
+LOCK TABLES `juegos` WRITE;
+/*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedidousuario`
+--
+
+DROP TABLE IF EXISTS `pedidousuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedidousuario` (
+  `idPedido` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(45) NOT NULL,
+  `articulo` varchar(45) NOT NULL,
+  PRIMARY KEY (`idPedido`),
+  UNIQUE KEY `idPedido_UNIQUE` (`idPedido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedidousuario`
+--
+
+LOCK TABLES `pedidousuario` WRITE;
+/*!40000 ALTER TABLE `pedidousuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedidousuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `precios`
+--
+
+DROP TABLE IF EXISTS `precios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `precios` (
+  `idArticulo` varchar(20) NOT NULL,
+  `precio` int NOT NULL,
+  PRIMARY KEY (`idArticulo`),
+  UNIQUE KEY `idArticulo_UNIQUE` (`idArticulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `precios`
+--
+
+LOCK TABLES `precios` WRITE;
+/*!40000 ALTER TABLE `precios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `precios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `nombreUsuario` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `telefono` int DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `idUsuario_UNIQUE` (`idUsuario`),
+  UNIQUE KEY `nombreUsuario_UNIQUE` (`nombreUsuario`),
+  UNIQUE KEY `correo_UNIQUE` (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-21 18:25:54
--- AngularGames/src/app/models/Juego.ts
+++ AngularGames/src/app/models/Juego.ts
@@ -1,4 +1,20 @@
 export class Juego{
+  idJuego:string;
+  nombre:string;
+  tipo:string;
+  jugadoresMin:string;
+  jugadoresMax:string;
+  dificultad:string;
 
-  
+constructor(idJuego:string,nombre:string,tipo:string,jugadoresMin:string,jugadoresMax:string,dificultad:string){
+
+  this.idJuego=idJuego;
+  this.nombre=nombre;
+  this.tipo=tipo;
+  this.jugadoresMin=jugadoresMin;
+  this.jugadoresMax=jugadoresMax;
+  this.dificultad=dificultad;
+
+}
+
 }
