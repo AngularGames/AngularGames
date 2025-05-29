f0640124762ae59d98d8f231c6fcb00fea226010
JesusLacera <lacerajesus@gmail.com>
2025-05-29 09:40:27 +0200

    29/05/25 Commit para hacer Fetch de el fix

--- NestAngularGames/angular-games-nest/src/main.ts
+++ NestAngularGames/angular-games-nest/src/main.ts
@@ -5,14 +5,14 @@ import { Juego } from './Model/Juego';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+ /* const config = new DocumentBuilder()
 .setTitle('Articulos API')
 .setDescription('Control de Artículos de Angular Games')
 .setVersion('1.0')
 .addTag("")
 .build();
 const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('ayuda/api', app, document);
+SwaggerModule.setup('ayuda/api', app, document);*/
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
