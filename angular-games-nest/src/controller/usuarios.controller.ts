d8132e4196042f116f51122d538a8ba710763c27
JesusLacera <lacerajesus@gmail.com>
2025-06-01 20:26:04 +0200

    01/06/25 Alta correct

--- angular-games-nest/src/controller/usuarios.controller.ts
+++ angular-games-nest/src/controller/usuarios.controller.ts
@@ -1,28 +1,30 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Query,
-  Res
-  
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { UsuariosService } from '../service/usuarios.service';
 import { Response } from 'express';
+import { Usuario } from 'src/Model/Usuario';
 @Controller('usuarios')
 export class UsuariosController {
-  constructor(private readonly usuariosService:UsuariosService) {}
+  constructor(private readonly usuariosService: UsuariosService) {}
 
-  @Get("validar")
-  async validarUsuario(@Query("usuario") usuario:string,@Query("password") password:string, @Res() response:Response){
-    const resultado:boolean = await this.usuariosService.validarUsuario(usuario,password);
+  @Get('validar')
+  async validarUsuario(
+    @Query('usuario') usuario: string,
+    @Query('password') password: string,
+    @Res() response: Response,
+  ) {
+    const resultado: boolean = await this.usuariosService.validarUsuario(
+      usuario,
+      password,
+    );
     if (resultado) {
-      response.status(200).send("USUARIO CORRECTO");
+      response.status(200).send('USUARIO CORRECTO');
     } else {
-      response.status(419).send("ERROR: USUARIO O CONTRASEÑA INCORRECTO")
+      response.status(419).send('ERROR: USUARIO O CONTRASEÑA INCORRECTO');
     }
-
-
   }
 
-
+  @Post('registro')
+  create(@Body() @Query('usuario') usuario: Usuario): void {
+    this.usuariosService.crearUsuario(usuario);
+  }
 }
--- angular-games-nest/src/service/usuarios.service.ts
+++ angular-games-nest/src/service/usuarios.service.ts
@@ -5,28 +5,38 @@ import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class UsuariosService {
-
   constructor(
     @InjectRepository(Usuario)
     private usuariosRepository: Repository<Usuario>,
   ) {}
-    async validarUsuario(usuarioBuscado:string,password:string):Promise<boolean>{
-      const resultado:Usuario = await this.usuariosRepository.findOneBy({nombreUsuario:usuarioBuscado,password:password})
-      if(resultado.nombreUsuario==usuarioBuscado&&resultado.password==password){ //creo que es redundante
-      return true
-    }else{ return false
-      
+  async validarUsuario(
+    usuarioBuscado: string,
+    password: string,
+  ): Promise<boolean> {
+    const resultado: Usuario = await this.usuariosRepository.findOneBy({
+      nombreUsuario: usuarioBuscado,
+      password: password,
+    });
+    if (
+      resultado.nombreUsuario == usuarioBuscado &&
+      resultado.password == password
+    ) {
+      //creo que es redundante
+      return true;
+    } else {
+      return false;
     }
   }
 
-      crearUsuario(usuario:Usuario):Promise<Usuario>{
-       return this.usuariosRepository.save(usuario);
-      }
-    
-      eliminarUsuario(usuario:string):Promise<DeleteResult>{
-        //cambio el map por el delete, que el repositorio no tiene map.
-        const resultado = this.usuariosRepository.delete({nombreUsuario:usuario});
-        console.log("la eliminación ha sido "+resultado);
-        return resultado;
-      }
-    }
+  crearUsuario(usuario: Usuario): Promise<Usuario> {
+    return this.usuariosRepository.save(usuario);
+  }
+
+  eliminarUsuario(usuario: string): Promise<DeleteResult> {
+    const resultado = this.usuariosRepository.delete({
+      nombreUsuario: usuario,
+    });
+    console.log('la eliminación ha sido ' + resultado);
+    return resultado;
+  }
+}
