1f5158990e4550f05db73d178cadf2f902f75c9d
JesusLacera <lacerajesus@gmail.com>
2025-06-09 17:28:04 +0200

    09/06/25

--- AngularGames/src/app/component/pagina-usuario/pagina-usuario.component.ts
+++ AngularGames/src/app/component/pagina-usuario/pagina-usuario.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../../models/usuario';
+import { UsuarioService } from '../../services/usuarios/usuario.service';
 
 @Component({
   selector: 'app-pagina-usuario',
   imports: [],
   templateUrl: './pagina-usuario.component.html',
-  styleUrl: './pagina-usuario.component.css'
+  styleUrl: './pagina-usuario.component.css',
 })
 export class PaginaUsuarioComponent {
+  nombre: string;
+  password: string;
+  usuarios: Usuario[] = [];
+  constructor(private usuarioService: UsuarioService) {}
 
+  nuevoUsuario(): Observable<Usuario> {
+    return this.usuarioService.crearUsuario();
+  }
 }
---
+++ AngularGames/src/app/models/navVar.ts
@@ -0,0 +1,4 @@
+export class navVar {
+  busqueda: string;
+  constructor(busqueda?: string) {}
+}
