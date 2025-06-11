d4799f5aafa8e0689ec48bc638ca74e44c6eee7e
JesusLacera <lacerajesus@gmail.com>
2025-06-11 09:53:25 +0200

    11/06/25

--- AngularGames/src/app/app.routes.ts
+++ AngularGames/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { PaginaAlmacenComponent } from './component/pagina-almacen/pagina-almacen.component';
 import { NuevoRegistroComponent } from './component/pagina-almacen/nuevo-registro/nuevo-registro.component';
 import { PaginaProductoComponent } from './component/pagina-producto/pagina-producto.component';
+import { PaginaUsuarioComponent } from './component/pagina-usuario/pagina-usuario.component';
 
 export const routes: Routes = [
   {path:"almacen", component:PaginaAlmacenComponent},
   {path:"nuevoProducto", component:NuevoRegistroComponent},
   {path:"producto", component:PaginaProductoComponent},
-
+  {path:"usuario",component:PaginaUsuarioComponent}
 
 
 ];
